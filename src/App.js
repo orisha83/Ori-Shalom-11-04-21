b8993e2bf73afcc318fb61a2225e7b46f7e058f3
Ori Shalom <shalom.ori@gmail.com>
2021-04-13 16:56:00 +0300

    ghpages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState} from "react";
+import { BrowserRouter as Router, Route, Switch, Redirect,} from "react-router-dom";
+import routes from './configs/routes'
+import FavoritesComp from './components/FavoritesComp'
+import HomeComp from './components/HomeComp'
+import HeaderComp from './components/HeaderComp'
+import { useDispatch } from "react-redux";
+import {getUserGeoLocation, getWeatherByCity} from './outerApiCalls/weatherServices'
+import {ToastContainer, toast} from "react-toastify";
 
 function App() {
+
+  const dispatch = useDispatch();  
+
+  useEffect(() => {
+    console.log("useEffect - dispatch")
+    const getUserWeather = async () =>  {
+      try{
+        const userLocationData = await getUserGeoLocation();
+        if(userLocationData) 
+        {
+          const weatherData = await getWeatherByCity(userLocationData.Key, userLocationData.LocalizedName)
+          dispatch({type:'LOAD_WEATHER_DATA',payload:weatherData});
+        }
+        else
+        {
+          const weatherData = await getWeatherByCity(215854, "Tel Aviv")
+          dispatch({type:'LOAD_WEATHER_DATA',payload:weatherData});
+        }
+      }
+      catch (err) {
+        toast.error("Can't set initial Location " + err.message);
+      }
+    };
+    getUserWeather();
+  },[dispatch]);
+
+
+
+
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HeaderComp />
+      <Switch>
+        <Route path={routes.favorites} component={FavoritesComp}></Route>
+        <Route exact path={routes.home} component={HomeComp}></Route>
+        <Redirect to="/" />
+      </Switch>
+      <ToastContainer />
     </div>
+    </Router>
   );
 }
 
